30a6feec3182918b2a8de56ba7e7727c0bf9b2aa
Nazmul Hasnat <nhasnat24@gmail.com>
2025-04-28 23:49:09 +0600

    Class and object practice

---
+++ Class_Objects_2.dart
@@ -0,0 +1,32 @@
+void main(){
+  
+  StudentInfo studentinfo = StudentInfo();
+
+  studentinfo.Name = "Nazmul Hasnat";
+  studentinfo.Roll = 222006;
+  studentinfo.Section = "C";
+  studentinfo.Grade = "A-";
+
+  studentinfo.displayInfo();
+
+}
+
+class StudentInfo{
+
+  String ? Name;
+  int ? Roll;
+  String ? Section;
+  String ? Grade;
+
+  void displayInfo(){
+
+
+   print("---- Student Information ----"); 
+   print("Name : $Name"); 
+   print("Roll : $Roll"); 
+   print("Section : $Section"); 
+   print("Grade : $Grade");
+
+
+  }
+}
