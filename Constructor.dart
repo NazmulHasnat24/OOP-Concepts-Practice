3bdf19d172cfc83b2eec90cc10cd853196ad5ea2
Nazmul Hasnat <nhasnat24@gmail.com>
2025-05-02 21:05:13 +0600

    Constructor Practice

--- Constructor.dart
+++ Constructor.dart
@@ -1,14 +1,21 @@
-import 'dart:js_interop';
-
 main(){
 
 Student info = Student();
 info.Name = "Esti";
 info.Id = 222006;
 info.display();
+
+
 }
 
 class Student{
+
+  Student(){
+    
+      //Constructor    
+      print("It is being called automatically while creating a constructor");
+  
+  }
 
   String ? Name;
   int ? Id;
